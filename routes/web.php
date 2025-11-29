ec34011e01f7e34c789e16a5b52359fc964d69af
ahmed777ismail <ahmed1kaballo@gmail.com>
2025-11-29 16:25:14 +0200

    feat: Initialize e-commerce project with core frontend assets, Laravel views, and help documentation.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product', function () {
+    return view('product');
+});
+
+Route::get('/category', function () {
+    return view('category');
+});
